2569a4d60980ec0be1e3c8c8e7885ba2b914055e
Erick-34 <erickoliveira2060@gmail>
2020-08-12 17:33:01 -0300

    feat: add keywords, new operation and buttons

--- src/Calculadora.js
+++ src/Calculadora.js
@@ -1,35 +1,69 @@
 import React, { useState } from 'react'
-
 import Button from './Button'
 
-export default function Calculadora(){
+export default function Calculadora() {
 
     const [display, setDisplay] = useState("");
 
-    const keyboard = [7, 8, 9, 4, 5, 6, 1, 2, 3, 0]
+    const calcRegex = /^([0-9]|-|\+|\*|\/|\.)*$/;
 
-    const handleNumberClick = (value) => {
-        setDisplay(`${display}${value}`)
-    }
-    
-    const handleDelete = () => {
-        setDisplay("")
-    }
+    const keyboard = [
+        7, 8, 9, "/", "C",
+        4, 5, 6, "*", "Del",
+        1, 2, 3, "-", "",
+        0, ".", "+", "=",
+    ]
 
-    const handleDeleteOne = () => {
-        const value = display.length;
-        setDisplay(display.substr(0, value -1))
+    const handleClick = (value) => {
+        switch (value) {
+            case "=":
+                setDisplay(eval(display))
+                break;
+            case "C":
+                setDisplay('')
+                break;
+            case "Del":
+                setDisplay(display.substr(0, display.length - 1))
+                break;
+            default:
+                setDisplay(`${display}${value}`)
+        }
     }
 
     return (
         <div className="calculadora">
-            <div className="display">
-                {display}
-            </div>
-            <div  className="keyboard">
-                { keyboard.map((value) => <Button key={value} value={value} SetState={handleNumberClick} /> )}
-                <button onClick={() => handleDeleteOne()}>Delete</button>
-                <button onClick={() => handleDelete()}>C</button>
+            <input
+                type="text"
+                value={display}
+                className="display"
+                onChange={event => {
+                    const { value } = event.target;
+                    if (calcRegex.test(value)) {
+                        setDisplay(value);
+                    }
+                }}
+                onKeyPress={event => {
+                    if (event.key === 'Enter') {
+                        setDisplay(eval(display))
+                    } else if (event.key.toLowerCase() === 'c') {
+                        setDisplay("")
+                    }
+                }}
+            />
+            <div className="keyboard">
+                {keyboard.map((value) => {
+                    const span2Class = value === 0 ? "span2" : '';
+                    const primaryClass = isNaN(value) ? "primay" : '';
+                    return (
+                        <Button
+                            key={value}
+                            value={value}
+                            SetState={handleClick}
+                            className={`${span2Class} ${primaryClass}`}
+                        />
+                    )
+                }
+                )}
             </div>
         </div>
     )
